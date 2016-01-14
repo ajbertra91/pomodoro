864a343f0f5d20f38d9e2c78f0422f8624c45aed
Adam <adam@sportstechinc.com>
2016-01-14 16:56:31 -0500

    adds base project files for pomodoro clock project

---
+++ app/components/pomodoro.js
@@ -0,0 +1,15 @@
+/*
+ * dependencies
+ */
+import jQuery from 'jquery';
+/*
+ * stylesheets
+ */
+require('./pomodoro.styl');
+/*
+ * module code
+ */
+const $ = jQuery;
+
+
+//$buttonEls.on( 'click', handleButton );
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  context: __dirname + "/app"
+  ,entry: './index'
+  ,output: {
+    path: __dirname + "/app"
+    ,filename: "bundle.js"
+  }
+  ,devtool: 'eval-source-map'
+  ,module: {
+    loaders: [
+      { 
+        test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"
+        ,query: { presets: ['es2015'] }
+      }
+      ,{ test: /\.styl$/, exclude: /node_modules/, loader: "style!css!stylus"}
+      ,{ test: /\.png$/, exclude: /node_modules/, loader: "url"}
+      ,{ test: /\.gif$/, exclude: /node_modules/, loader: "url"}
+    ]
+    ,resolve: {
+      extensions: ['.js']
+    }
+  }
+  ,watch: true
+};
